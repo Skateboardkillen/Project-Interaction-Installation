d478def87d26f29c7eecc54457c01a76efcd8728
Vincent <snowboardkille@gmail.com>
2026-06-04 13:14:35 +0200

    Fixed local paths not being found on vercel

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3000;
 
-// Set EJS as the template engine
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-// Serve static files from the public directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index', { 
