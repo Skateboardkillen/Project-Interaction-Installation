68dcd4c60880d0fe13db36218746e85f0efcf7a8
Vincent <snowboardkille@gmmail.com>
2026-06-04 11:21:52 +0200

    Added theme and waitingView

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const PORT = 3000;
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
 
+// Serve static files from the public directory
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.render('index', { 
         username: 'User', 
